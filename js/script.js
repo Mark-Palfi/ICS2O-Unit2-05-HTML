e73f3c9ec03b32d64bf6dc7c87865a57697d6031
Mark-Palfi <99292598+Mark-Palfi@users.noreply.github.com>
2022-03-25 20:25:32 +0000

    calculate

--- js/script.js
+++ js/script.js
@@ -3,13 +3,17 @@
 // This file contains the JS functions for index.html
 
 /**
- * Do basic math.
+ * This function gets users street name and number and shows it back to user.
  */
-function doMathClicked() {
+function enterClicked() {
+  // input
+  const street_name = document.getElementById("hours").value
+  const street_number = parseInt(document.getElementById("salary").value)
 
-document.getElementById('area').innerHTML = '<p>area = ' + (6 + 5) + '</p>'
-  document.getElementById('perimeter').innerHTML = '<p>perimeter =  ' + ((5 + 3) * 2) + '</p>'
-  document.getElementById('multiply-math').innerHTML = '<p>3 + 4 × 2 = ' + (3 + 4 * 2) + '</p>'
-  document.getElementById('divide-math').innerHTML = '<p>(4 ÷ 2) + 3 = ' + ((4 / 2) + 3) + '</p>'
-  document.getElementById('exponent-math').innerHTML = '<p>5 + 2³ = ' + (5 + 2 ** 3) + '</p>'
+  // math
+    document.getElementById('pay').innerHTML = '<p>pay = ' + ("hours" * "salary") * (1.00 - 0.18) + '</p>'
+  document.getElementById('taxes').innerHTML = '<p>taxes =  ' + ("hours" * "salary") * (0.18)) + '</p>'    
+  // output
+  document.getElementById("address").innerHTML =
+    "You live on: " + street_name + ", house " + street_number + "."
 }
